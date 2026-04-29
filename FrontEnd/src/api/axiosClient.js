51ac192463e910377fa4039332677c25eb20d542
ton-cong <vucongton9@gmail.com>
2026-04-29 08:54:30 +0700

    chore: setup env variable for api url

--- FrontEnd/src/api/axiosClient.js
+++ FrontEnd/src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
     headers: {
         'Content-Type': 'application/json',
     },
